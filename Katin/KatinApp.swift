dfbaabc2510aed55a8be01b5588bd779fab69bd5
Ochanyoro <wadaoo0223@icloud.com>
2022-07-05 21:28:27 +0900

    Initial Commit

---
+++ Katin/KatinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KatinApp.swift
+//  Katin
+//
+//  Created by 大和田一裕 on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct KatinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
